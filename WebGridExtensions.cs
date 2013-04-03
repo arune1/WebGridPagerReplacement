235bd691c4875351f5d2dbe3d5273ac7a3622f59
Daniel Smith <danielrsmith@gmail.com>
2013-04-03 15:04:00 -0300

    Modified to not show first, last, next, prev when they don't need to appear

--- WebGridExtensions.cs
+++ WebGridExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Web.Helpers;
@@ -29,7 +29,7 @@ public static class WebGridExtensions
         int numericLinksCount,
         bool explicitlyCalled)
     {
-        
+
         int currentPage = webGrid.PageIndex;
         int totalPages = webGrid.PageCount;
         int lastPage = totalPages - 1;
@@ -37,111 +37,124 @@ public static class WebGridExtensions
         var ul = new TagBuilder("ul");
         var li = new List<TagBuilder>();
 
-        
-        if (ModeEnabled(mode, WebGridPagerModes.FirstLast)) {
-            if (String.IsNullOrEmpty(firstText)) {
+
+        if (ModeEnabled(mode, WebGridPagerModes.FirstLast))
+        {
+            if (String.IsNullOrEmpty(firstText))
+            {
                 firstText = "First";
             }
 
-            var part = new TagBuilder("li") {
+            var part = new TagBuilder("li")
+            {
                 InnerHtml = GridLink(webGrid, webGrid.GetPageUrl(0), firstText)
             };
 
-            if (currentPage == 0) {
-                part.MergeAttribute("class", "disabled");
+            if (currentPage != 0)
+            {
+                li.Add(part);
             }
 
-            li.Add(part);
 
         }
-        
-        if (ModeEnabled(mode, WebGridPagerModes.NextPrevious)) {
-            if (String.IsNullOrEmpty(previousText)) {
+
+        if (ModeEnabled(mode, WebGridPagerModes.NextPrevious))
+        {
+            if (String.IsNullOrEmpty(previousText))
+            {
                 previousText = "Prev";
             }
 
-            int page = currentPage == 0 ? 0: currentPage - 1;
+            int page = currentPage == 0 ? 0 : currentPage - 1;
 
-            var part = new TagBuilder("li") {
+            var part = new TagBuilder("li")
+            {
                 InnerHtml = GridLink(webGrid, webGrid.GetPageUrl(page), previousText)
             };
-            
-            if (currentPage == 0) {
-                part.MergeAttribute("class", "disabled");
+
+            if (currentPage != 0)
+            {
+                li.Add(part);
             }
-
-            li.Add(part);
-
         }
 
 
-       if (ModeEnabled(mode, WebGridPagerModes.Numeric) && (totalPages > 1)) {
+        if (ModeEnabled(mode, WebGridPagerModes.Numeric) && (totalPages > 1))
+        {
             int last = currentPage + (numericLinksCount / 2);
             int first = last - numericLinksCount + 1;
-            if (last > lastPage) {
+            if (last > lastPage)
+            {
                 first -= last - lastPage;
                 last = lastPage;
             }
-            if (first < 0) {
+            if (first < 0)
+            {
                 last = Math.Min(last + (0 - first), lastPage);
                 first = 0;
             }
-            for (int i = first; i <= last; i++) {
+            for (int i = first; i <= last; i++)
+            {
 
                 var pageText = (i + 1).ToString(CultureInfo.InvariantCulture);
-                var part = new TagBuilder("li") {
+                var part = new TagBuilder("li")
+                {
                     InnerHtml = GridLink(webGrid, webGrid.GetPageUrl(i), pageText)
                 };
 
-                if (i == currentPage) {
+                if (i == currentPage)
+                {
                     part.MergeAttribute("class", "active");
                 }
-                
+
                 li.Add(part);
 
             }
         }
-        
-        if (ModeEnabled(mode, WebGridPagerModes.NextPrevious)) {
-            if (String.IsNullOrEmpty(nextText)) {
+
+        if (ModeEnabled(mode, WebGridPagerModes.NextPrevious))
+        {
+            if (String.IsNullOrEmpty(nextText))
+            {
                 nextText = "Next";
             }
-            
-            int page = currentPage == lastPage ? lastPage: currentPage + 1;
 
-            var part = new TagBuilder("li") {
+            int page = currentPage == lastPage ? lastPage : currentPage + 1;
+
+            var part = new TagBuilder("li")
+            {
                 InnerHtml = GridLink(webGrid, webGrid.GetPageUrl(page), nextText)
             };
-            
-            if (currentPage == lastPage) {
-                part.MergeAttribute("class", "disabled");
+
+            if (!(currentPage == lastPage || totalPages == 1))
+            {
+                li.Add(part);
             }
-
-            li.Add(part);
-
         }
-        
-        if (ModeEnabled(mode, WebGridPagerModes.FirstLast)) {
-            if (String.IsNullOrEmpty(lastText)) {
+
+        if (ModeEnabled(mode, WebGridPagerModes.FirstLast))
+        {
+            if (String.IsNullOrEmpty(lastText))
+            {
                 lastText = "Last";
             }
 
-            var part = new TagBuilder("li") {
+            var part = new TagBuilder("li")
+            {
                 InnerHtml = GridLink(webGrid, webGrid.GetPageUrl(lastPage), lastText)
             };
-            
-            if (currentPage == lastPage) {
-                part.MergeAttribute("class", "disabled");
+
+            if (!(currentPage == lastPage || totalPages == 1))
+            {
+                li.Add(part);
             }
-
-            li.Add(part);
-
         }
 
         ul.InnerHtml = string.Join("", li);
 
         var html = "";
-        if (explicitlyCalled && webGrid.IsAjaxEnabled) {
+        if (explicitlyCalled && webGrid.IsAjaxEnabled)
+        {
             var span = new TagBuilder("span");
             span.MergeAttribute("data-swhgajax", "true");
             span.MergeAttribute("data-swhgcontainer", webGrid.AjaxUpdateContainerId);
@@ -150,21 +163,25 @@ public static class WebGridExtensions
             span.InnerHtml = ul.ToString();
             html = span.ToString();
 
-        } else {
+        }
+        else
+        {
             html = ul.ToString();
         }
 
-        return new HelperResult(writer => {
-            writer.Write(html);         
+        return new HelperResult(writer =>
+        {
+            writer.Write(html);
         });
     }
 
-    private static String GridLink(WebGrid webGrid, string url, string text) 
+    private static String GridLink(WebGrid webGrid, string url, string text)
     {
         TagBuilder builder = new TagBuilder("a");
         builder.SetInnerText(text);
         builder.MergeAttribute("href", url);
-        if (webGrid.IsAjaxEnabled) {
+        if (webGrid.IsAjaxEnabled)
+        {
             builder.MergeAttribute("data-swhglnk", "true");
         }
         return builder.ToString(TagRenderMode.Normal);
